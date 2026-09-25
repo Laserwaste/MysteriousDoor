0af576c876ce4301121602498431c3f6bae3ea23
Laserwaste <permataalkautsar1@gmail.com>
2026-09-25 13:47:54 +0700

    Initial commit

---
+++ MysteriousDoor/Program.cs
@@ -0,0 +1,11 @@
+﻿// A Character is trying to unlock a Mysterious door in this dungeon
+//The door Has a secret code = 1992
+//The Cahracter keeps trying different code until the correct one is entered
+
+String SecretCode = "1992";
+string attempt = "";
+
+while(attempt != SecretCode)
+{
+    
+}
